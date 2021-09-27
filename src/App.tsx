286eec0c24ee226c751ba7a90b3695896e67f5c1
Tim Maas <tim.maas@conclusion.nl>
2021-09-27 15:31:30 +0200

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewto",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <main>
+        <Title content="Some Simple Title" />
+        {all_pokemon.map((pokemon) => {
+          return (
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          );
+        })}
+      </main>
     </div>
   );
 }
